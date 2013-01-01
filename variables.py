2068daa2ccdfad911d464fcbc65e3254c41093eb
Hasan Tareque <hmtareque@gmail.com>
2013-01-01 13:08:36 +0000

    basic variable

---
+++ variables.py
@@ -0,0 +1,23 @@
+""" 
+    Variables
+    - Variable names must only be one word (as in no spaces)
+    - Variable names must be made up of only letters, numbers, and underscore (_)
+    - Variable names cannot begin with a number
+"""
+
+my_string = 'Hello, World!'
+my_int = 8
+my_float = 45.06
+my_bool = 5 > 9 #A Boolean value will return either True or False
+my_list = ['item_1', 'item_2', 'item_3', 'item_4']
+my_tuple = ('one', 'two', 'three')
+my_dict = {'letter': 'g', 'number': 'seven', 'symbol': '&'}
+
+print(my_string)
+print(my_int)
+print(my_float)
+print(my_bool)
+print(my_list)
+print(my_tuple)
+print(my_dict)
+
