313a47da34f8d5dacb52d49068f607ffd4579105
Hasan Tareque <hmtareque@gmail.com>
2013-01-03 18:02:14 +0000

    python info

---
+++ intro/main.py
@@ -0,0 +1,7 @@
+"""
+    Python is a cross-platform programming language.
+    It is free and open-source.
+    Even though most of today's Linux and Mac have Python pre-installed in it.
+"""
+
+print('Hello python')
