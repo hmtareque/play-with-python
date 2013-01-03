dc247d8bdd034d05d1e99db91a382cbea9dbc69c
Hasan Tareque <hmtareque@gmail.com>
2013-01-03 15:50:51 +0000

    explored math lib

--- maths/math-lib.py
+++ maths/math-lib.py
@@ -1,3 +1,43 @@
 # math lib 
 import math 
 
+# important functions form math lib 
+
+# return the ceiling of x
+print(math.ceil(7.5)) 
+
+# return the floor of x
+print(math.floor(7.5)) 
+
+#return the absolute value of x.
+print(math.fabs(-9.12))
+
+#return x factorial as an integer.
+print(math.factorial(70))
+
+# return fmod(x, y), as defined by the platform C library.
+print(math.fmod(10, 4))
+
+# return an accurate floating point sum of values in the iterable.
+print(math.fsum([8,9.4,8.765,3,765]))
+
+# return True if x is a positive or negative infinity, and False otherwise.
+print(math.isinf(5))
+
+# Power and logarithmic functions
+
+# return e raised to the power x, where e = 2.718281
+print(math.exp(8))
+
+# return x raised to the power y.
+print(math.pow(3, 2))
+
+# return the square root of x.
+print(math.sqrt(7))
+
+
+# The mathematical constant π = 3.141592, to available precision.
+print(math.pi)
+
+# The mathematical constant e = 2.718281, to available precision.
+print(math.e)
