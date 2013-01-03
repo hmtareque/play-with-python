800b2c7cc62f0dadc133e48a47827bcce18ab284
Hasan Tareque <hmtareque@gmail.com>
2013-01-03 22:32:36 +0000

    python identifier rules are now identified

---
+++ intro/identifiers.py
@@ -0,0 +1,22 @@
+#Python Identifiers
+"""
+An identifier is a name given to entities like class, functions, variables, etc. 
+It helps to differentiate one entity from another.
+"""
+
+#Rules for writing identifiers
+""" 
+- Identifiers can be a combination of letters in lowercase (a to z) or uppercase (A to Z) or digits (0 to 9) or an underscore _. Names like myClass, var_1 and print_this_to_screen, all are valid example.
+- An identifier cannot start with a digit. 1variable is invalid, but variable1 is a valid name.
+- Keywords cannot be used as identifiers.
+- We cannot use special symbols like !, @, #, $, % etc. in our identifier.
+- An identifier can be of any length.
+"""
+
+#Things to Remember
+"""
+- Python is a case-sensitive language. This means, Variable and variable are not the same.
+- Always give the identifiers a name that makes sense. While c = 10 is a valid name, writing count = 10 would make more sense, and it would be easier
+  to figure out what it represents when you look at your code after a long gap.
+- Multiple words can be separated using an underscore, like this_is_a_long_variable.
+"""
---
+++ intro/keywords.py
@@ -0,0 +1,44 @@
+"""
+    - Keywords are the reserved words in Python.
+    - We cannot use a keyword as a variable name, function name or any other identifier. 
+    - They are used to define the syntax and structure of the Python language.
+    - In Python, keywords are case sensitive.
+    - There are 33 keywords in Python 3.7. This number can vary slightly over the course of time.
+    - All the keywords except True, False and None are in lowercase and they must be written as they are. 
+"""
+
+# True
+# False
+# None
+# await
+# import
+# pass
+# break
+# except
+# in
+# raise
+# class
+# finally
+# is
+# return
+# and
+# continue
+# for
+# lambda
+# try
+# as
+# def
+# from
+# nonlocal
+# while
+# assert
+# del
+# global
+# not
+# with
+# async
+# elif
+# if
+# else
+# or
+# yield
