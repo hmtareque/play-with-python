090b986a11e746bc9a7a2b71fbee109079ddd446
Hasan Tareque <hmtareque@gmail.com>
2013-01-04 09:45:20 +0000

    constants values never change

---
+++ intro/constants.py
@@ -0,0 +1,22 @@
+"""A constant is a type of variable whose value cannot be changed. 
+It is helpful to think of constants as containers that hold information 
+which cannot be changed later."""
+
+"""We can think of constants as a bag to store some books which cannot be 
+replaced once placed inside the bag."""
+
+"""Assigning value to constant in Python
+In Python, constants are usually declared and assigned in a module. 
+Here, the module is a new file containing variables, functions, etc 
+which is imported to the main file. Inside the module, constants are written in 
+all capital letters and underscores separating the words."""
+
+
+# Create a file constants.py:
+# import constant
+
+PI = 3.14
+GRAVITY = 9.8
+
+print(PI)
+print(GRAVITY)
