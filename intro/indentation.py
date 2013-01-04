512e5c8ae7ebd6c3348f2ff70445223e9410d441
Hasan Tareque <hmtareque@gmail.com>
2013-01-04 23:44:54 +0000

    indentation makes code nice

---
+++ intro/indentation.py
@@ -0,0 +1,23 @@
+"""Most of the programming languages like C, C++, and Java use braces { } to define a block of code.
+Python, however, uses indentation."""
+
+"""A code block (body of a function, loop, etc.) starts with indentation and 
+ends with the first unindented line. The amount of indentation is up to you, but it must be 
+consistent throughout that block."""
+
+"""Generally, four whitespaces are used for indentation and are preferred over tabs."""
+
+# Here is an example.
+
+for i in range(1, 10):
+    print(i)
+    if i == 5:
+        print("Hasan is great!")
+
+a = 10
+if a != 5:
+    print("Elizaaa ...")
+
+"""The enforcement of indentation in Python makes the code look neat and clean. 
+This results in Python programs that look similar and consistent."""
+
