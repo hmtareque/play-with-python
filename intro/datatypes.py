8eee79376173433ebf19696add62ea02bf51cd72
Hasan Tareque <hmtareque@gmail.com>
2013-01-05 15:24:36 +0000

    and here is the list

--- intro/datatypes.py
+++ intro/datatypes.py
@@ -12,4 +12,20 @@ a = 1+2j
 print(a, "is complex number?", isinstance(1+2j,complex))
 
 
+# Python List
+"""List is an ordered sequence of items. It is one of the most used datatype in Python and is very flexible. 
+All the items in a list do not need to be of the same type."""
 
+"""Declaring a list is pretty straight forward. 
+Items separated by commas are enclosed within brackets [ ]."""
+
+a = [5,10,15,20,25,30,35,40]
+
+# a[2] = 15
+print("a[2] = ", a[2])
+
+# a[0:3] = [5, 10, 15]
+print("a[0:3] = ", a[0:3])
+
+# a[5:] = [30, 35, 40]
+print("a[5:] = ", a[5:])
