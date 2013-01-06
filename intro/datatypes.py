11a1242779eb98b078915fa856fcf08f9aec019b
Hasan Tareque <hmtareque@gmail.com>
2013-01-06 15:27:01 +0000

    tuple is the immutable list

--- intro/datatypes.py
+++ intro/datatypes.py
@@ -29,3 +29,15 @@ print("a[0:3] = ", a[0:3])
 
 # a[5:] = [30, 35, 40]
 print("a[5:] = ", a[5:])
+
+# Python Tuple
+"""Tuple is an ordered sequence of items same as a list. 
+The only difference is that tuples are immutable. Tuples once created cannot be modified."""
+
+t = (5,'program', 1+3j)
+
+# t[1] = 'program'
+print("t[1] = ", t[1])
+
+# t[0:3] = (5, 'program', (1+3j))
+print("t[0:3] = ", t[0:3])
