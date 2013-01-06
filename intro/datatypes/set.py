fc77d3116379076104a413237c56998168f504dd
Hasan Tareque <hmtareque@gmail.com>
2013-01-06 16:12:42 +0000

    set is an unordered list

---
+++ intro/datatypes/set.py
@@ -0,0 +1,17 @@
+"""Set is an unordered collection of unique items. 
+Set is defined by values separated by comma inside braces { }. 
+Items in a set are not ordered."""
+
+a = {5,2,3,1,4}
+
+# printing set variable
+print("a = ", a)
+
+# data type of variable a
+print(type(a))
+
+"""We can perform set operations like union, intersection on two sets. 
+Sets have unique values. They eliminate duplicates."""
+
+a = {1,2,2,3,3,3}
+print(a)
