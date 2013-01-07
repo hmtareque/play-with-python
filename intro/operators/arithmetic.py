68225c7b4023386f6e7b5c2aae5c3cf6b88ebe00
Hasan Tareque <hmtareque@gmail.com>
2013-01-07 18:32:53 +0000

    arithmethic operators

---
+++ intro/operators/arithmetic.py
@@ -0,0 +1,23 @@
+"""Arithmetic operators are used to perform mathematical operations 
+like addition, subtraction, multiplication, etc."""
+
+x = 15
+y = 4
+
+# Output: x + y = 19
+print('x + y =',x+y)
+
+# Output: x - y = 11
+print('x - y =',x-y)
+
+# Output: x * y = 60
+print('x * y =',x*y)
+
+# Output: x / y = 3.75
+print('x / y =',x/y)
+
+# Output: x // y = 3
+print('x // y =',x//y)
+
+# Output: x ** y = 50625
+print('x ** y =',x**y)
