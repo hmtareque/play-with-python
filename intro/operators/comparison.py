bea37020e3848c354415da7b21531c60dccd965a
Hasan Tareque <hmtareque@gmail.com>
2013-01-07 18:33:28 +0000

    comparison operators

---
+++ intro/operators/comparison.py
@@ -0,0 +1,23 @@
+"""Comparison operators are used to compare values. 
+It returns either True or False according to the condition."""
+
+x = 10
+y = 12
+
+# Output: x > y is False
+print('x > y is',x>y)
+
+# Output: x < y is True
+print('x < y is',x<y)
+
+# Output: x == y is False
+print('x == y is',x==y)
+
+# Output: x != y is True
+print('x != y is',x!=y)
+
+# Output: x >= y is False
+print('x >= y is',x>=y)
+
+# Output: x <= y is True
+print('x <= y is',x<=y)
