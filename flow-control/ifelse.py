a7bd095960ead6f6ff6f05bef285ef264cfd2c97
Hasan Tareque <hmtareque@gmail.com>
2013-01-08 01:05:03 +0000

    flow control with ifelifelse

--- flow-control/ifelse.py
+++ flow-control/ifelse.py
@@ -26,3 +26,17 @@ if num >= 0:
     print("Positive or Zero")
 else:
     print("Negative number")
+
+'''In this program, 
+we check if the number is positive or
+negative or zero and 
+display an appropriate message'''
+
+num = -3.4
+
+if num > 0:
+    print("Positive number")
+elif num == 0:
+    print("Zero")
+else:
+    print("Negative number")
