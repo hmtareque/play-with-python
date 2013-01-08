b22e646336d0746c7c2aac18e047199ed4d6841e
Hasan Tareque <hmtareque@gmail.com>
2013-01-08 00:57:13 +0000

    flow control with if

---
+++ flow-control/ifelse.py
@@ -0,0 +1,14 @@
+""""Decision making is required when we want to execute a code only if a certain condition is satisfied."""
+
+
+# If the number is positive, we print an appropriate message
+
+num = 3
+if num > 0:
+    print(num, "is a positive number.")
+print("This is always printed.")
+
+num = -1
+if num > 0:
+    print(num, "is a positive number.")
+print("This is also always printed.")
