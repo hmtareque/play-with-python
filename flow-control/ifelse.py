48c4fa26ccf2013717f220317ca848eaa0ac7770
Hasan Tareque <hmtareque@gmail.com>
2013-01-08 01:06:33 +0000

    flow control with nested ifelse

--- flow-control/ifelse.py
+++ flow-control/ifelse.py
@@ -40,3 +40,18 @@ elif num == 0:
     print("Zero")
 else:
     print("Negative number")
+
+'''In this program, we input a number
+check if the number is positive or
+negative or zero and display
+an appropriate message
+This time we use nested if statement'''
+
+num = float(input("Enter a number: "))
+if num >= 0:
+    if num == 0:
+        print("Zero")
+    else:
+        print("Positive number")
+else:
+    print("Negative number")
