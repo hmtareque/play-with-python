e2beacfd2c6a3b2b4f59fd255afcbfdd22a1ecbd
Hasan Tareque <hmtareque@gmail.com>
2013-01-09 01:33:09 +0000

    for loop with else

--- flow-control/loop.py
+++ flow-control/loop.py
@@ -14,3 +14,22 @@ for val in numbers:
 
 print("The sum is", sum)
 
+
+# Program to iterate through a list using indexing
+
+genre = ['pop', 'rock', 'jazz']
+
+# iterate over the list using index
+for i in range(len(genre)):
+	print("I like", genre[i])
+
+# for loop with else
+
+digits = [0, 1, 5]
+
+for i in digits:
+    print(i)
+else:
+    print("No items left.")
+
+
