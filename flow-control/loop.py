b96fbad1b539656be6347122701e737b55ebc5ae
Hasan Tareque <hmtareque@gmail.com>
2013-01-09 01:20:18 +0000

    for loop

---
+++ flow-control/loop.py
@@ -0,0 +1,16 @@
+"""Iterate over a sequence of elements using the different variations of for loop."""
+
+# Program to find the sum of all numbers stored in a list
+
+# List of numbers
+numbers = [6, 5, 3, 8, 4, 2, 5, 4, 11]
+
+# variable to store the sum
+sum = 0
+
+# iterate over the list
+for val in numbers:
+	sum = sum+val
+
+print("The sum is", sum)
+
