51d75967b2074fc1eed26bd956bbf65d758132d8
Hasan Tareque <hmtareque@gmail.com>
2013-01-11 02:06:28 +0000

    pass statement

---
+++ flow-control/pass.py
@@ -0,0 +1,17 @@
+"""In Python programming, the pass statement is a null statement. 
+The difference between a comment and a pass statement in Python is that while the interpreter 
+ignores a comment entirely, pass is not ignored."""
+
+'''pass is just a placeholder for
+functionality to be added later.'''
+sequence = {'p', 'a', 's', 's'}
+for val in sequence:
+    pass
+
+# We can do the same thing in an empty function or class as well.
+
+def function(args):
+    pass
+
+class Example:
+    pass
