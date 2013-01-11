6117fb1693db5bf3ff87368e451ab2ebb15f8242
Hasan Tareque <hmtareque@gmail.com>
2013-01-11 11:32:24 +0000

    functions

---
+++ functions/main.py
@@ -0,0 +1,29 @@
+"""In Python, a function is a group of related statements that performs a specific task."""
+
+"""A function definition that consists of the following components."""
+
+"""Keyword def that marks the start of the function header."""
+
+"""A function name to uniquely identify the function. Function 
+naming follows the same rules of writing identifiers in Python."""
+
+"""Parameters (arguments) through which we pass values to a function. They are optional."""
+
+"""A colon (:) to mark the end of the function header."""
+
+"""Optional documentation string (docstring) to describe what the function does."""
+
+"""One or more valid python statements that make up the function body. 
+Statements must have the same indentation level (usually 4 spaces)."""
+
+"""An optional return statement to return a value from the function."""
+
+def greet(name):
+    """
+    This function greets to
+    the person passed in as
+    a parameter
+    """
+    print("Hello, " + name + ". Good morning!")
+
+greet("Hasan")
