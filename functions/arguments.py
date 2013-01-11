6329d07da682af02c3853611d12c7ffbb3bd4a41
Hasan Tareque <hmtareque@gmail.com>
2013-01-11 21:21:40 +0000

    can have default argument

--- functions/arguments.py
+++ functions/arguments.py
@@ -7,3 +7,22 @@ def greet(name, msg):
     print("Hello", name + ', ' + msg)
 
 greet("Monica", "Good morning!")
+
+# Function arguments can have default values in Python.
+
+def greet(name, msg="Good morning!"):
+    """
+    This function greets to
+    the person with the
+    provided message.
+
+    If the message is not provided,
+    it defaults to "Good
+    morning!"
+    """
+
+    print("Hello", name + ', ' + msg)
+
+
+greet("Kate")
+greet("Bruce", "How do you do?")
