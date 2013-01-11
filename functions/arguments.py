38359f70171f670c25d2398ed0d8d4e1d928782a
Hasan Tareque <hmtareque@gmail.com>
2013-01-11 21:25:31 +0000

    arbitrary argument

--- functions/arguments.py
+++ functions/arguments.py
@@ -40,3 +40,19 @@ greet(msg = "How do you do?", name = "Bruce")
 
 # 1 positional, 1 keyword argument
 greet("Bruce", msg = "How do you do?") 
+
+# Python Arbitrary Arguments
+"""Sometimes, we do not know in advance the number of arguments that will 
+be passed into a function. Python allows us to handle this kind of situation 
+through function calls with an arbitrary number of arguments."""
+
+def greet(*names):
+    """This function greets all
+    the person in the names tuple."""
+
+    # names is a tuple with arguments
+    for name in names:
+        print("Hello", name)
+
+
+greet("Monica", "Luke", "Steve", "John")
