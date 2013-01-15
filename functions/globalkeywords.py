a79e2c647fdb96b27d950b6d19a18f9c676d848e
Hasan Tareque <hmtareque@gmail.com>
2013-01-15 00:30:51 +0000

    global keyword in nested func

--- functions/globalkeywords.py
+++ functions/globalkeywords.py
@@ -7,3 +7,21 @@ def add():
 
 add()
 print("In main:", c)
+
+
+""" in nested functions """ 
+
+def foo():
+    x = 20
+
+    def bar():
+        global x
+        x = 25
+    
+    print("Before calling bar: ", x)
+    print("Calling bar now")
+    bar()
+    print("After calling bar: ", x)
+
+foo()
+print("x in main: ", x)
